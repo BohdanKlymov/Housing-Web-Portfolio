412d889c1ca024a3e0f29c632ee0efb6c1212cf1
Bohdan.Klymov <programmer.b.k.339@gmail.com>
2026-01-02 20:34:35 +0100

    Fix Flask static and template folder paths

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+import os
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(
+        __name__,
+        static_folder=os.path.join(os.path.dirname(__file__), "static"),
+        template_folder=os.path.join(os.path.dirname(__file__), "templates")
+    )
 
     from .routes import main
     app.register_blueprint(main)
