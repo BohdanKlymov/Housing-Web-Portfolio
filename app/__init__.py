840cd64e9f1901a06b77e955aaf0a5fc388e56c8
Bohdan.Klymov <programmer.b.k.339@gmail.com>
2026-01-03 13:33:04 +0100

    Use default Flask static and template folders

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,10 @@
 from flask import Flask
-import os
 
 def create_app():
-    app = Flask(
-        __name__,
-        static_folder=os.path.join(os.path.dirname(__file__), "static"),
-        template_folder=os.path.join(os.path.dirname(__file__), "templates")
-    )
+    app = Flask(__name__)
 
     from .routes import main
     app.register_blueprint(main)
 
     return app
+
